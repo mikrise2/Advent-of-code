2c244f70c978aa8782964aa3b9f69903ddb3d73e
mikrsie2 <mikrise2@mail.ru>
2022-12-04 00:15:58 +0100

    completed first day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,46 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxSum = Int.MIN_VALUE
+        var tempSum = 0
+        for (index in input.indices) {
+            when {
+                input[index] == "" -> {
+                    maxSum = max(tempSum, maxSum)
+                    tempSum=0
+                }
+                index == input.size - 1 -> {
+                    maxSum = max(tempSum+input[index].toInt(), maxSum)
+                }
+                else -> {
+                    tempSum+=input[index].toInt()
+                }
+            }
+        }
+        return maxSum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val bests = arrayOf(Int.MIN_VALUE, Int.MIN_VALUE, Int.MIN_VALUE)
+        var tempSum = 0
+        for (index in input.indices) {
+            when {
+                input[index] == "" -> {
+                    bests[0] = max(tempSum,  bests[0])
+                    bests.sort()
+                    tempSum=0
+                }
+                index == input.size - 1 -> {
+                    bests[0] = max(tempSum+input[index].toInt(), bests[0])
+                }
+                else -> {
+                    tempSum+=input[index].toInt()
+                }
+            }
+        }
+        return bests.sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
